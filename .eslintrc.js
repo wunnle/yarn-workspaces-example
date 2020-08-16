ba1be74b1ea66b4b08b212c80a5e6d68a33313f1
Sinan Aksay <me@wunnle.com>
2020-08-16 18:30:26 +0300

    added prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,36 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es2020": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 11
-    },
-    "rules": {
-    }
+  env: {
+    browser: true,
+    commonjs: true,
+    es2020: true
+  },
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 11
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        trailingComma: 'none',
+        singleQuote: true,
+        printWidth: 90
+      }
+    ],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    quotes: [
+      2,
+      'single',
+      {
+        avoidEscape: true,
+        allowTemplateLiterals: true
+      }
+    ],
+    'comma-dangle': 0,
+    'simple-import-sort/sort': 'error',
+    'max-len': 0,
+    'multiline-ternary': ['error', 'always-multiline'],
+    'no-console': 0
+  }
 };
--- packages/packageB/index.js
+++ packages/packageB/index.js
@@ -1,3 +1,3 @@
-const { getDateNameOfToday } = require('package-a')
+const {   getDateNameOfToday } = require('package-a')
 
 console.log(`today is ${getDateNameOfToday()}!`)
